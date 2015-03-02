454d1f46bc43acec25d29a21baf5fc81519d7bd8
MacRat <m@crat.jp>
2015-03-02 20:45:51 +0900

    webkit_web_viewを試験的に追加。とりあえずググれるようになったよ。 一応今後を見越してboxの中に入っている。

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <unistd.h>
+#include <gtk/gtk.h>
+#include <webkit2/webkit2.h>
+
+GtkWidget *g_webview;
+
+int main(int argc, char **argv)
+{
+	GtkWidget *window, *box, *scrolled;
+
+	gtk_init(&argc, &argv);
+
+	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+	box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+	gtk_container_add(GTK_CONTAINER(window), box);
+
+	scrolled = gtk_scrolled_window_new(NULL, NULL);
+	gtk_box_pack_start(GTK_BOX(box), scrolled, TRUE, TRUE, 0);
+
+	g_webview = webkit_web_view_new();
+	gtk_container_add(GTK_CONTAINER(scrolled), g_webview);
+
+	webkit_web_view_load_uri(WEBKIT_WEB_VIEW(g_webview), "http://google.com/");
+
+	g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_widget_show_all(window);
+	gtk_main();
+
+	return 0;
+}
